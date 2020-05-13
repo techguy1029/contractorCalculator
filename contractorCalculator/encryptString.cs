1b37c568f6cb8614552142eab94dcf4edebde155
techguy1029 <43223653+techguy1029@users.noreply.github.com>
2020-05-12 19:30:48 -0700

    Add files via upload
    -Lists all projects

---
+++ contractorCalculator/encryptString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace contractorCalculator
+{
+    public static class encryptString
+    {
+        public static byte[] GetHash(string inputString)
+        {
+            using(HashAlgorithm algorithm = SHA256.Create())
+            {
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+            }
+        }
+    }
+}
