38969cb21464332b805deef8c4cc9a65668ab2a6
techguy1029 <43223653+techguy1029@users.noreply.github.com>
2020-05-25 22:18:51 -0700

    Search Good

---
+++ contractorCalculator/contractorCalculator/convertInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contractorCalculator
+{
+    public static class convertInput
+    {
+        public static decimal stringToDecimal(TextBox textBox)
+        {
+            string textBoxText = textBox.Text;
+            decimal textBoxTextToDecimal;
+            decimal.TryParse(textBoxText,out textBoxTextToDecimal);
+            return textBoxTextToDecimal;
+        }
+
+        public static string stringToMoney(string money)
+        {
+            return "$" + money;
+        }
+    }
+}
